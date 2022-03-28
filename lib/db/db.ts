4f712328561e6fcf5bbcac7b52fbf867c1ba9420
Max <beppe@live.se>
2022-03-28 19:14:55 +1100

    feat: fetch teams

--- lib/db/db.ts
+++ lib/db/db.ts
@@ -1,3 +1,8 @@
+import { client } from './client';
+
 export async function listTeams({}) {
   // fetch teams from database
+  const { data, error } = await client.from('teams').select();
+  if (error) throw error;
+  return data;
 }
