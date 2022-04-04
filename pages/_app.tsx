ae0228cb61d3f13dc808c3f17acded1a3bd5d808
Max <beppe@live.se>
2022-04-04 18:48:00 +1000

    feat: add layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@theme/theme-context';
+import { Layout } from '@components/templates/layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   );
 }
