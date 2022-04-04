529b7b8ebf50b0d3ee3ef807b72b8b04324ce734
Max <beppe@live.se>
2022-04-04 18:47:04 +1000

    feat: wrap chakra menu

---
+++ components/organisms/menu.tsx
@@ -0,0 +1,19 @@
+import {
+  Menu as ChakraMenu,
+  MenuButton as ChakraMenuButton,
+  MenuList as ChakraMenuList,
+  MenuItem as ChakraMenuItem,
+  MenuItemOption as ChakraMenuItemOption,
+  MenuGroup as ChakraMenuGroup,
+  MenuOptionGroup as ChakraMenuOptionGroup,
+  MenuDivider as ChakraMenuDivider,
+} from '@chakra-ui/react';
+
+export const Menu = ChakraMenu;
+export const MenuButton = ChakraMenuButton;
+export const MenuList = ChakraMenuList;
+export const MenuItem = ChakraMenuItem;
+export const MenuItemOption = ChakraMenuItemOption;
+export const MenuGroup = ChakraMenuGroup;
+export const MenuOptionGroup = ChakraMenuOptionGroup;
+export const MenuDivider = ChakraMenuDivider;
