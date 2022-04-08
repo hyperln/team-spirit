bb16c147699c873da4e46e2346761aaae4b08194
Max <beppe@live.se>
2022-04-08 19:41:12 +1000

    feat: add select

---
+++ components/molecules/avatar-image.tsx
@@ -0,0 +1,9 @@
+import {
+  Avatar as ChakraAvatar,
+  AvatarBadge as ChakraAvatarBadge,
+  AvatarGroup as ChakraAvatarGroup,
+} from '@chakra-ui/react';
+
+export const Avatar = ChakraAvatar;
+export const AvatarBadge = ChakraAvatarBadge;
+export const AvatarGroup = ChakraAvatarGroup;
