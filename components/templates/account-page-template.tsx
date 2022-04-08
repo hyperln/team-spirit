ebda634f42b32ed8013731f900ffe59b7da6f215
Max <beppe@live.se>
2022-04-08 19:46:40 +1000

    feat: allow saving profile data

--- components/templates/account-page-template.tsx
+++ components/templates/account-page-template.tsx
@@ -1,91 +1,139 @@
+import { useContext, useEffect, useState } from 'react';
 import { useToast } from '@hooks/use-toast';
 import { Box } from '@components/atoms/box';
 import { Button } from '@components/atoms/button';
 import { Input } from '@components/atoms/input';
-import { Text } from '@components/atoms/typography/text';
 import { useAuth } from '@hooks/use-auth';
-import { currentUser } from '@lib/auth';
-import { client } from '@lib/auth/client';
-import { useEffect, useState } from 'react';
-
+import { updateUserProfile } from '@lib/db';
+import { Select } from '@components/atoms/select';
+import { Flex } from '@components/atoms/flex';
+import { uploadAvatarImage } from '@lib/storage/storage';
+import { ProfileContext } from '@state/profile-context';
+import { Avatar } from '@components/molecules/avatar-image';
 
 export function AccountPageTemplate() {
-
   const toast = useToast();
-  const { user } = useAuth()
-  const [ image, setImage ] = useState(null) 
-  const [ firstname, setFirstname] = useState("")
-  const [ lastname, setLastname] = useState("")
-  const [ avatarurl, setAvatarUrl] = useState("")
+  const { user } = useAuth();
+  const [image, setImage] = useState(null);
+  const [previewImageUrl, setPreviewImageUrl] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [gender, setGender] = useState('');
 
-  
-  const handleSubmit = async(e) => {
-    e.preventDefault()
+  const {
+    state: { profile },
+    actions: { fetchProfile },
+  } = useContext(ProfileContext);
 
-    let avatarUrl = ""
-    if(image) {
-      const {data, error} = await client.storage.from("avatars").upload(`${Date.now()}_${image.name}`, image)
-      
-      if(error) {
-        console.log(error)
-      }
+  useEffect(() => {
+    if (profile) {
+      setFirstName(profile.firstName);
+      setLastName(profile.lastName);
+      setGender(profile.gender);
+      setPreviewImageUrl(profile.previewUrl);
+    }
+  }, [profile]);
 
-      if(data) {
-        setAvatarUrl(data.Key)
-        avatarUrl = data.Key
-      }
+  const onSelectFile = (e) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setPreviewImageUrl(undefined);
+      return;
     }
 
-    const {data, error} = await client.from("profiles").upsert( {
-      id: user.id,
-      first_name: firstname,
-      last_name: lastname,
-      avatar_url: avatarurl,
-      updated_at: new Date()
-      })
+    const selectedFile = e.target.files[0];
 
-      if(error) {
+    const objectUrl = URL.createObjectURL(selectedFile);
+    setPreviewImageUrl(objectUrl);
+    setImage(selectedFile);
+  };
+
+  const handleGenderSelect = (e) => {
+    setGender(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let avatarUrl = '';
+
+    if (image) {
+      try {
+        const avatarUrlData = await uploadAvatarImage(user.id, image);
+        avatarUrl = avatarUrlData.Key.split('avatars/')[1];
+      } catch (error) {
         return toast({
           status: 'error',
           description: error.message,
-          title: 'error'
+          title: 'error',
         });
       }
-    
-      if(data) {
-        return toast ({
-          status: 'success',
-          description: 'Profile has been updated!',
-          title: 'Success'  
-      });
-  }
-}
-  return (
-    <Box>
-      Welcome, {user?.email}
-      
-      <Input 
-      type="text" 
-      placeholder="First Name" 
-      onChange={e => setFirstname(e.target.value)} 
-      value={firstname} />
-      
-      <Input 
-      type="text" 
-      placeholder="Last Name" 
-      onChange={e => setLastname(e.target.value)} 
-      value={lastname}/>
-      
-      <Input 
-      type="file" 
-      placeholder="profile pic" 
-      accept="image/*" 
-      onChange={e => setImage(e.target.files)} />
+    }
 
-      <Button onClick={handleSubmit} >
-      Save Profile
-      </Button>
-    </Box>
+    try {
+      await updateUserProfile(user.id, {
+        firstName,
+        lastName,
+        avatarUrl,
+        gender,
+      });
+
+      fetchProfile();
+
+      return toast({
+        status: 'success',
+        description: 'Profile has been updated!',
+        title: 'Success',
+      });
+    } catch (error) {
+      return toast({
+        status: 'error',
+        description: error.message,
+        title: 'error',
+      });
+    }
+  };
+
+  return (
+    <Flex justifyContent="center">
+      <Box>
+        Welcome, {profile?.firstName || user.email}
+        <form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            placeholder="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+            value={firstName}
+            name="firstName"
+          />
+          <Input
+            type="text"
+            placeholder="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+            value={lastName}
+            name="lastName"
+          />
+          <Select
+            onChange={handleGenderSelect}
+            placeholder="gender"
+            variant="flushed"
+            value={gender}
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </Select>
+          <Input
+            type="file"
+            placeholder="Profile picture"
+            accept="image/*"
+            onChange={onSelectFile}
+            name="avatar"
+          />
+          {previewImageUrl ? (
+            <Avatar src={previewImageUrl} name={firstName || user.email} />
+          ) : null}
+          <Button type="submit">Save Profile</Button>
+        </form>
+      </Box>
+    </Flex>
   );
 }
-
