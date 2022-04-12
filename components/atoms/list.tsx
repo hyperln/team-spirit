7946276a1eebe7c50f2f081c3052fdd79e1ec1d6
Christian <christian.torlegard@outlook.com>
2022-04-12 20:21:07 +0200

    util: created Lists as chakralists .

---
+++ components/atoms/list.tsx
@@ -0,0 +1,13 @@
+import {
+  List as ChakraList,
+  ListItem as ChakraListItem,
+  ListIcon as ChakraListIcon,
+  OrderedList as ChakraOrderedList,
+  UnorderedList as ChakraUnorderedList,
+} from '@chakra-ui/react';
+
+export const List = ChakraList;
+export const ListItem = ChakraListItem;
+export const ListIcon = ChakraListIcon;
+export const OrderedList = ChakraOrderedList;
+export const UnorderedList = ChakraUnorderedList;
