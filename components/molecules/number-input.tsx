7c58c7c42fbd02bd96decda75fbff6fd1e1d5ea3
Max <beppe@live.se>
2022-04-13 18:27:58 +1000

    feat: add number input

---
+++ components/molecules/number-input.tsx
@@ -0,0 +1,13 @@
+import {
+  NumberInput as ChakraNumberInput,
+  NumberInputField as ChakraNumberInputField,
+  NumberInputStepper as ChakraNumberInputStepper,
+  NumberIncrementStepper as ChakraNumberIncrementStepper,
+  NumberDecrementStepper as ChakraNumberDecrementStepper,
+} from '@chakra-ui/react';
+
+export const NumberInput = ChakraNumberInput;
+export const NumberInputField = ChakraNumberInputField;
+export const NumberInputStepper = ChakraNumberInputStepper;
+export const NumberIncrementStepper = ChakraNumberIncrementStepper;
+export const NumberDecrementStepper = ChakraNumberDecrementStepper;
