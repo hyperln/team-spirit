76573754b1bd678017ec101cd856117a173f0265
Christian <christian.torlegard@outlook.com>
2022-04-14 09:58:55 +0200

    feat:

---
+++ pages/clubs/[clubId].tsx
@@ -0,0 +1,7 @@
+import { withRequireAuth } from '@hoc/with-auth';
+import withTransition from '@hoc/with-transition';
+
+function ClubDetails() {
+  return <></>;
+}
+export default withTransition(withRequireAuth(ClubDetails));
