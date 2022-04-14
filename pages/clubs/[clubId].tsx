8cdacb411b557369e66365142db16739bea5d47a
Max <beppe@live.se>
2022-04-14 18:16:30 +1000

    feat: fetch club data server side and pass to template

--- pages/clubs/[clubId].tsx
+++ pages/clubs/[clubId].tsx
@@ -1,7 +1,35 @@
+import Head from 'next/head';
+import { ClubPageTemplate } from '@components/templates/club-page-template';
 import { withRequireAuth } from '@hoc/with-auth';
 import withTransition from '@hoc/with-transition';
+import { fetchClub } from '@lib/db';
+import { Club } from 'shared/types';
 
-function ClubDetails() {
-  return <></>;
+interface Props {
+  club: Club;
 }
+
+function ClubDetails({ club }: Props) {
+  return (
+    <>
+      <Head>
+        <title>{club.name} | Team Spirit</title>
+      </Head>
+      <ClubPageTemplate club={club} />
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { clubId } = context.query;
+  const club = await fetchClub(clubId);
+
+  return {
+    props: {
+      clubId,
+      club,
+    },
+  };
+}
+
 export default withTransition(withRequireAuth(ClubDetails));
