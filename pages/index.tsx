d6ac1f97afdb4020bb0a9237fc1b10208ec16ef9
Christian <christian.torlegard@outlook.com>
2022-04-15 12:21:17 +0200

    refactor: removed auth href on startpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,6 @@ function Home(): ReactElement {
       <main>
         <Box w="full">
           <Flex flexDirection="column" alignItems="center" w="full" mx="auto">
-            <Link href="/authed">Authed page</Link>
             <Link href="/account">Account</Link>
             <Link href="/clubs">View clubs</Link>
             <Link href="/clubs/club-registration">Register club</Link>
