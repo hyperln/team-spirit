f23c59bb6531face92b118ec743a484330d257b8
Christian <christian.torlegard@outlook.com>
2022-04-15 11:35:07 +0200

    feat: WIP: Drawer for menu navigation

--- components/organisms/navigation.tsx
+++ components/organisms/navigation.tsx
@@ -4,70 +4,94 @@ import {
   LockIcon,
   StarIcon,
 } from '@chakra-ui/icons';
-import { IconButton } from '@components/atoms/button';
-import { Center } from '@components/atoms/center';
-import { Flex } from '@components/atoms/flex';
+import { DrawerCloseButton } from '@chakra-ui/react';
+import { Button, IconButton } from '@components/atoms/button';
 import { Link } from '@components/atoms/link';
 import { Avatar } from '@components/molecules/avatar-image';
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuGroup,
-} from '@components/organisms/menu';
+import { Menu, MenuItem, MenuGroup } from '@components/organisms/menu';
 import { useAuth } from '@hooks/use-auth';
+import { useDisclosure } from '@hooks/use-disclosure';
 import { useProfile } from '@hooks/use-profile';
+import { useState } from 'react';
+import { Drawer, DrawerBody, DrawerContent, DrawerOverlay } from './drawer';
 
 export function Navigation() {
   const { profile } = useProfile();
   const { signOut } = useAuth();
+
+  const [size, setSize] = useState('full');
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleClick = (newSize) => {
+    setSize(newSize);
+    onOpen();
+  };
+
   return (
     <Menu placement="bottom">
-      <MenuButton
+      <Button
         as={IconButton}
         aria-label="Options"
         icon={<HamburgerIcon />}
         variant="outline"
-      />
-      <nav>
-        <MenuList>
-          <MenuGroup
-            title={`Welcome${
-              profile?.firstName ? `, ${profile.firstName}` : ''
-            }`}
-          >
-            <MenuItem
-              as={Link}
-              href="/account"
-              icon={
-                profile ? (
-                  <Avatar
-                    size="xs"
-                    src={profile.previewUrl}
-                    name={profile.firstName}
-                  />
-                ) : (
-                  <AtSignIcon />
-                )
-              }
+        onClick={() => handleClick(size)}
+        key={size}
+        m={4}
+      >
+        {`Open ${size} Drawer`}
+      </Button>
+
+      <Drawer onClose={onClose} isOpen={isOpen} size={size}>
+        <DrawerOverlay />
+        <DrawerContent alignItems="center">
+          <DrawerCloseButton />
+          <DrawerBody>
+            <MenuGroup
+              title={`Welcome${
+                profile?.firstName ? `, ${profile.firstName}` : ''
+              }`}
             >
-              Account
-            </MenuItem>
-
-            <MenuItem as={Link} href="/" icon={<StarIcon w={6} />}>
-              Home
-            </MenuItem>
-            <MenuItem as={Link} href="/clubs" icon={<StarIcon w={6} />}>
-              Clubs
-            </MenuItem>
-
-            <MenuItem onClick={signOut} icon={<LockIcon w={6} />}>
-              Log out
-            </MenuItem>
-          </MenuGroup>
-        </MenuList>
-      </nav>
+              <MenuItem
+                as={Link}
+                href="/account"
+                onClick={onClose}
+                icon={
+                  profile ? (
+                    <Avatar
+                      size="xs"
+                      src={profile.previewUrl}
+                      name={profile.firstName}
+                    />
+                  ) : (
+                    <AtSignIcon />
+                  )
+                }
+              >
+                Account
+              </MenuItem>
+              <MenuItem
+                as={Link}
+                href="/"
+                icon={<StarIcon w={6} />}
+                onClick={onClose}
+              >
+                Home
+              </MenuItem>
+              <MenuItem
+                as={Link}
+                href="/clubs"
+                icon={<StarIcon w={6} />}
+                onClick={onClose}
+              >
+                Clubs
+              </MenuItem>
+              <MenuItem onClick={signOut} icon={<LockIcon w={6} />}>
+                Log out
+              </MenuItem>
+            </MenuGroup>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </Menu>
   );
 }
