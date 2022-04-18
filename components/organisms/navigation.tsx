fe3752e6f1dfa78995740e0e19f540680a207c27
Max <beppe@live.se>
2022-04-18 13:39:13 +1000

    feat: add links to nav and add toggle dark mode

--- components/organisms/navigation.tsx
+++ components/organisms/navigation.tsx
@@ -2,7 +2,9 @@ import {
   AtSignIcon,
   HamburgerIcon,
   LockIcon,
+  MoonIcon,
   StarIcon,
+  SunIcon,
 } from '@chakra-ui/icons';
 import { IconButton } from '@components/atoms/button';
 import { Center } from '@components/atoms/center';
@@ -17,11 +19,20 @@ import {
   MenuGroup,
 } from '@components/organisms/menu';
 import { useAuth } from '@hooks/use-auth';
+import { useColorMode } from '@hooks/use-color-mode';
 import { useProfile } from '@hooks/use-profile';
+
+const colorModeIcons = {
+  dark: SunIcon,
+  light: MoonIcon,
+};
 
 export function Navigation() {
   const { profile } = useProfile();
   const { signOut } = useAuth();
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const ColorModeIcon = colorModeIcons[colorMode];
   return (
     <Menu placement="bottom">
       <MenuButton
@@ -54,9 +65,36 @@ export function Navigation() {
             >
               Account
             </MenuItem>
+            <MenuItem
+              as={Link}
+              href="/account/payment-methods"
+              icon={<AtSignIcon w={6} />}
+            >
+              Payment Methods
+            </MenuItem>
+            <MenuItem as={Link} href="/clubs" icon={<AtSignIcon w={6} />}>
+              Clubs
+            </MenuItem>
+            <MenuItem
+              as={Link}
+              href="/clubs/club-registration"
+              icon={<AtSignIcon w={6} />}
+            >
+              Register club
+            </MenuItem>
+            <MenuItem
+              as={Link}
+              href="/account/payment-methods"
+              icon={<AtSignIcon w={6} />}
+            >
+              Payment Methods
+            </MenuItem>
 
             <MenuItem as={Link} href="/" icon={<StarIcon w={6} />}>
               Home
+            </MenuItem>
+            <MenuItem onClick={toggleColorMode} icon={<ColorModeIcon w={6} />}>
+              Switch to {colorMode === 'light' ? 'dark' : 'light'} mode
             </MenuItem>
             <MenuItem onClick={signOut} icon={<LockIcon w={6} />}>
               Log out
