c706d8bc487c58ceff9106e5a1cb4974446bc44e
Max <beppe@live.se>
2022-04-19 11:04:05 +1000

    feat: add back button to all pages

--- components/templates/layout.tsx
+++ components/templates/layout.tsx
@@ -1,8 +1,12 @@
+import { useRouter } from 'next/router';
 import { Box } from '@components/atoms/box';
 import { Flex } from '@components/atoms/flex';
 import { Navigation } from '@components/organisms/navigation';
+import { IconButton } from '@components/atoms/button';
+import { ArrowBackIcon } from '@chakra-ui/icons';
 
 export function Layout({ children }) {
+  const router = useRouter();
   return (
     <Box>
       <Flex
@@ -18,10 +22,18 @@ export function Layout({ children }) {
       >
         <Navigation />
       </Flex>
+      <Box m="2" display={{ lg: 'none' }}>
+        <IconButton
+          variant="unstyled"
+          aria-label="Back"
+          onClick={() => router.back()}
+          icon={<ArrowBackIcon />}
+        />
+      </Box>
       <Box
         minH="calc(100vh - 80px)"
         mb={{ base: '20', lg: undefined }}
-        mt={{ lg: '20' }}
+        mt={{ base: '12', lg: '20' }}
       >
         {children}
       </Box>
