f6d1dc214ff98265150f60b222ac3d5feb6fa56e
Max <beppe@live.se>
2022-04-19 17:56:57 +1000

    feat: drawer stuff

--- components/organisms/navigation.tsx
+++ components/organisms/navigation.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   AtSignIcon,
   HamburgerIcon,
@@ -6,18 +7,15 @@ import {
   StarIcon,
   SunIcon,
 } from '@chakra-ui/icons';
+
 import { Button, IconButton } from '@components/atoms/button';
-import { Box } from '@components/atoms/box';
-import { Center } from '@components/atoms/center';
 import { Flex } from '@components/atoms/flex';
 import { Link } from '@components/atoms/link';
 import { Avatar } from '@components/molecules/avatar-image';
-import { Menu, MenuItem, MenuGroup } from '@components/organisms/menu';
 import { useAuth } from '@hooks/use-auth';
 import { useDisclosure } from '@hooks/use-disclosure';
 import { useColorMode } from '@hooks/use-color-mode';
 import { useProfile } from '@hooks/use-profile';
-import { useState } from 'react';
 import {
   Drawer,
   DrawerBody,
@@ -44,10 +42,15 @@ export function Navigation() {
     onOpen();
   };
 
+  const handlClickLogout = () => {
+    signOut();
+    onClose();
+  };
+
   const ColorModeIcon = colorModeIcons[colorMode];
 
   return (
-    <Menu placement="bottom">
+    <>
       <Button
         as={IconButton}
         aria-label="Options"
@@ -70,58 +73,71 @@ export function Navigation() {
           <DrawerCloseButton />
           <DrawerBody>
             <nav>
-              <Flex flexDir="column" mt="20" mx="10">
-                <MenuItem as={Link} href="/" icon={<StarIcon w={6} />}>
+              <Flex alignItems="flex-start" flexDir="column" mt="20" mx="10">
+                <Link display="flex" gap="4" alignItems="center" href="/">
+                  <StarIcon w={6} />
                   Home
-                </MenuItem>
-                <MenuItem
-                  as={Link}
+                </Link>
+                <Link
                   href="/account"
-                  icon={
-                    profile ? (
-                      <Avatar
-                        size="xs"
-                        src={profile.previewUrl}
-                        name={profile.firstName}
-                      />
-                    ) : (
-                      <AtSignIcon w={6} />
-                    )
-                  }
+                  display="flex"
+                  gap="4"
+                  alignItems="center"
                 >
+                  {profile ? (
+                    <Avatar
+                      size="xs"
+                      src={profile.previewUrl}
+                      name={profile.firstName}
+                    />
+                  ) : (
+                    <AtSignIcon w={6} />
+                  )}
                   Account
-                </MenuItem>
-                <MenuItem
-                  as={Link}
+                </Link>
+                <Link
                   href="/account/payment-methods"
-                  icon={<AtSignIcon w={6} />}
+                  display="flex"
+                  gap="4"
+                  alignItems="center"
                 >
+                  <AtSignIcon w={6} />
                   Payment Methods
-                </MenuItem>
-                <MenuItem as={Link} href="/clubs" icon={<AtSignIcon w={6} />}>
+                </Link>
+                <Link display="flex" gap="4" alignItems="center" href="/clubs">
+                  <AtSignIcon w={6} />
                   Clubs
-                </MenuItem>
-                <MenuItem
-                  as={Link}
+                </Link>
+                <Link
+                  display="flex"
+                  gap="4"
+                  alignItems="center"
                   href="/clubs/club-registration"
-                  icon={<AtSignIcon w={6} />}
                 >
+                  <AtSignIcon w={6} />
                   Register club
-                </MenuItem>
-                <MenuItem
+                </Link>
+                <Button
+                  fontWeight="normal"
+                  variant="unstyled"
                   onClick={toggleColorMode}
-                  icon={<ColorModeIcon w={6} />}
+                  leftIcon={<ColorModeIcon w={6} />}
                 >
                   Switch to {colorMode === 'light' ? 'dark' : 'light'} mode
-                </MenuItem>
-                <MenuItem onClick={signOut} icon={<LockIcon w={6} />}>
+                </Button>
+                <Button
+                  fontWeight="normal"
+                  variant="unstyled"
+                  onClick={handlClickLogout}
+                  leftIcon={<LockIcon w={6} />}
+                >
                   Log out
-                </MenuItem>
+                </Button>
               </Flex>
             </nav>
           </DrawerBody>
         </DrawerContent>
       </Drawer>
-    </Menu>
+    </>
   );
 }
