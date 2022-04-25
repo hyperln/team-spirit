284e2df01c040c44c8f166155f0a8bc3ec9c4d03
Max <beppe@live.se>
2022-04-25 16:56:42 +1000

    feat: add skeleton

---
+++ components/molecules/skeleton.tsx
@@ -0,0 +1,9 @@
+import {
+  Skeleton as ChakraSkeleton,
+  SkeletonCircle as ChakraSkeletonCircle,
+  SkeletonText as ChakraSkeletonText,
+} from '@chakra-ui/react';
+
+export const Skeleton = ChakraSkeleton;
+export const SkeletonCircle = ChakraSkeletonCircle;
+export const SkeletonText = ChakraSkeletonText;
