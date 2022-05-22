b298e0b0845720d1c060fa9efa9f165df235efca
Christian <christian.torlegard@outlook.com>
2022-05-22 20:11:51 +0200

    chore: Importing chakraAccordion

---
+++ components/molecules/accordion.tsx
@@ -0,0 +1,13 @@
+import {
+  Accordion as ChakraAccordion,
+  AccordionItem as ChakraAccordionItem,
+  AccordionButton as ChakraAccordionButton,
+  AccordionPanel as ChakraAccordionPanel,
+  AccordionIcon as ChakraAccordionIcon,
+} from '@chakra-ui/react';
+
+export const Accordion = ChakraAccordion;
+export const AccordionItem = ChakraAccordionItem;
+export const AccordionButton = ChakraAccordionButton;
+export const AccordionPanel = ChakraAccordionPanel;
+export const AccordionIcon = ChakraAccordionIcon;
