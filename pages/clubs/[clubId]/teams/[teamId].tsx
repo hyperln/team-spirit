af50808951f0555876f220f7a92a10ac684860ec
MattisKarlsson <Mattis.karlsson@outlook.com>
2022-06-02 11:31:45 +0200

    Feat: Club logo loaded in team page, colouring still not applied dynamically.

--- pages/clubs/[clubId]/teams/[teamId].tsx
+++ pages/clubs/[clubId]/teams/[teamId].tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import { withRequireAuth } from '@hoc/with-auth';
 import withTransition from '@hoc/with-transition';
-import { fetchTeam } from '@lib/db';
+import { fetchTeam, fetchClub } from '@lib/db';
 import { Club, Team } from 'shared/types';
 import { TeamPageTemplate } from '@components/templates/team-page-template';
+import { getLogoImage } from '@lib/storage/storage';
 
 interface Props {
   team: Team;
@@ -23,10 +24,16 @@ function TeamDetails({ team, club }: Props) {
 
 export async function getServerSideProps(context) {
   const { clubId, teamId = [] } = context.query;
-  const team = await fetchTeam(teamId);
+  const [team, club] = await Promise.all([
+    fetchTeam(teamId), 
+    fetchClub(clubId)
+  ]);
+
+  const { signedURL } = await getLogoImage(club.logoImageId);
 
   return {
     props: {
+      club: { logoUrl: signedURL },
       teamId,
       team,
       clubId,
